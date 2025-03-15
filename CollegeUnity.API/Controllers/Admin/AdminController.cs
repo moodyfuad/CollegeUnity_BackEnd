e6e257c0576f4ff545778876d1bd0cc031757018
Faisal-Bojeir <158216002+Faisal-Bojeir@users.noreply.github.com>
2025-03-16 00:02:41 +0300

    Complete the get admins for a community endpoint

--- CollegeUnity.API/Controllers/Admin/AdminController.cs
+++ CollegeUnity.API/Controllers/Admin/AdminController.cs
@@ -8,6 +8,7 @@ using CollegeUnity.Core.Dtos.CommunityDtos.Create;
 using CollegeUnity.Core.Dtos.CommunityDtos.Update;
 using CollegeUnity.Core.Dtos.QueryStrings;
 using CollegeUnity.Core.Dtos.ResponseDto;
+using CollegeUnity.Core.Dtos.StudentCommunityDtos.Get;
 using CollegeUnity.Core.Dtos.StudentServiceDtos;
 using CollegeUnity.Core.Dtos.SubjectDtos;
 using CollegeUnity.Core.Entities;
@@ -36,25 +37,6 @@ namespace CollegeUnity.API.Controllers.Admin
             _manageCommunityFeatures = manageCommunityFeatures;
         }
 
-        //[HttpGet("Staff")]
-        //public async Task<IActionResult> Get([Required][FromQuery(Name = "Name")] string name, StaffParameters staffParameters) 
-        //{
-        //    var response = await _serviceManager.AdminServices.SearchStaffBy(name,staffParameters);
-        //    HttpContext.Response.StatusCode = response.StatusCode;
-            
-        //    return new JsonResult(response); 
-        //}
-
-        //[HttpGet("SearchStudents")]
-        //public async Task<IActionResult> GetStudent([FromQuery] StudentSearchParameters parameters) 
-        //{
-        //    //return await new StudentController(serviceManager: _serviceManager);
-        //   /* var response = await _serviceManager.StudentServices.GetStudentsAsync(parameters);
-        //    HttpContext.Response.StatusCode = response.StatusCode;*/
-
-        //    return Ok("Not implemented"); 
-        //}
-
         [HttpPost("Staff/Create")]
         public async Task<IActionResult> CreateStaffAccount([FromForm] CreateStaffDto dto)
         {
@@ -68,8 +50,8 @@ namespace CollegeUnity.API.Controllers.Admin
             return new JsonResult(ApiResponse<bool?>.BadRequest("There is staff with the same email or phone, try again."));
         }
 
-        [HttpPut("Staff/Update/{staffId}")]
-        public async Task<IActionResult> UpdateStaffInfo(int staffId, [FromForm] UStaffDto dto)
+        [HttpPut("Staff/Update")]
+        public async Task<IActionResult> UpdateStaffInfo([FromHeader] int staffId, [FromForm] UStaffDto dto)
         {
             var isSuccess = await _manageStaffFeatures.UpdateStaffAccount(staffId, dto);
 
@@ -163,6 +145,14 @@ namespace CollegeUnity.API.Controllers.Admin
 
             return new JsonResult(ApiResponse<bool?>.BadRequest(isSuccess.message));
         }
+
+        [HttpGet("Community/Admins")]
+        public async Task<IActionResult> GetCommunityAdmins([FromQuery]GetStudentCommunityAdminsParameters parameters)
+        {
+            var admins = await _manageCommunityFeatures.GetAdmins(parameters);
+            return new JsonResult(ApiResponse<IEnumerable<GCommunityAdmins>>.Success(admins, pageNumber: null, pageSize: null));
+        }
+
 
         [HttpPost("Community/Update/{communityId}")]
         public async Task<IActionResult> UpdateCommunityInfo(int communityId, [FromForm] UCommunityInfoDto dto)
---
+++ CollegeUnity.Core/MappingExtensions/StudentCommunityExtensions/Get/GetCommunityAdminExtention.cs
@@ -0,0 +1,31 @@
+﻿using CollegeUnity.Core.Dtos.PostDtos.Get;
+using CollegeUnity.Core.Dtos.StudentCommunityDtos.Get;
+using CollegeUnity.Core.Entities;
+using CollegeUnity.Core.MappingExtensions.PostExtensions.Get;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeUnity.Core.MappingExtensions.StudentCommunityExtensions.Get
+{
+    public static class GetCommunityAdminExtention
+    {
+        public static GCommunityAdmins GetCommunityAdmins(this StudentCommunity studentCommunity)
+        {
+            var admin = new GCommunityAdmins 
+            {
+                Id = studentCommunity.StudentId,
+                FullName = $"{studentCommunity.Student.FirstName} {studentCommunity.Student.MiddleName} {studentCommunity.Student.LastName}",
+                Role = studentCommunity.Role != null ? (int?)studentCommunity.Role : null
+            };
+            return admin;
+        }
+
+        public static IEnumerable<GCommunityAdmins> ToCommunityAdminsMappers(this IEnumerable<StudentCommunity> studentCommunities)
+        {
+            return studentCommunities.Select(admin => admin.GetCommunityAdmins());
+        }
+    }
+}
