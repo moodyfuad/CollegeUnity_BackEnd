0d6d85d2af6d261e3ac48a429cf5f1eeb02759b0
Faisal-Bojeir <158216002+Faisal-Bojeir@users.noreply.github.com>
2025-05-27 02:53:11 +0300

    Complete

---
+++ Contract/AdminFeatures/Posts/IManagePostFeatures.cs
@@ -0,0 +1,16 @@
+﻿using CollegeUnity.Core.Dtos.PostDtos.Get;
+using CollegeUnity.Core.Dtos.QueryStrings;
+using CollegeUnity.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeUnity.Contract.AdminFeatures.Posts
+{
+    public interface IManagePostFeatures
+    {
+        //Task<PagedList<GPostDto>> GetPostsDetails(DateTime dateTime, GetPostsDetailsParameters parameters);
+    }
+}
